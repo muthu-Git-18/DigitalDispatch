7c01842687359f012f0c626564dc04522ea4808b
muthu <teamliftss987@gmail.com>
2026-09-09 11:01:18 +0530

    Dispatch Commit---muthu

--- app/src/main/java/com/united/digitaldispatch/Dispatch/Dispatch.kt
+++ app/src/main/java/com/united/digitaldispatch/Dispatch/Dispatch.kt
@@ -1,8 +1,10 @@
 package com.united.digitaldispatch.Dispatch
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import com.united.digitaldispatch.R
 
 class Dispatch : AppCompatActivity() {
@@ -12,9 +14,17 @@ class Dispatch : AppCompatActivity() {
         setContentView(R.layout.activity_dispatch)
 
         val btnBack = findViewById<ImageView>(R.id.btn_back)
+        val btnCreateHeader = findViewById<AppCompatButton>(R.id.btn_createHeader)
 
         btnBack.setOnClickListener {
             finish()
+        }
+
+
+
+        btnCreateHeader.setOnClickListener {
+            val intent = Intent(this@Dispatch, CreateHeaderDispatch::class.java)
+            startActivity(intent)
         }
     }
 }
